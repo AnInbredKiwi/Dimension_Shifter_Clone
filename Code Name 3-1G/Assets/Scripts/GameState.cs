f089c8e7a000bdbe308fa15a6039b0c277ad02f8
Boyan <tigara12@gmail.com>
2021-10-26 18:32:16 +0200

    Made cubes freeze and go to the same plane when you press Q and go back when you press it again

--- Code Name 3-1G/Assets/Scripts/GameState.cs
+++ Code Name 3-1G/Assets/Scripts/GameState.cs
@@ -10,34 +10,61 @@ public class GameState : MonoBehaviour
 
     Camera cam;
     Transform player;
+    GameObject[] Cubes;
+    float[] CubesZ;
 
     public float cam2DSize = 7f;
+
 
     void Awake()
     {
         state3D = true;
         cam = FindObjectOfType<Camera>();
         player = GameObject.FindGameObjectWithTag("Player").transform;
+        Cubes = GameObject.FindGameObjectsWithTag("Cube");
+        CubesZ = new float[Cubes.Length];
+        
+
     }
 
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.Q))
         {
             state3D = !state3D;
             cam.orthographic = !cam.orthographic;
             cam.GetComponent<CinemachineBrain>().enabled = !cam.GetComponent<CinemachineBrain>().enabled;
+            Rigidbody rb;
+
+            if(state3D)
+            {
+                for (int cubeIndex = 0; cubeIndex < Cubes.Length; cubeIndex++)
+                {
+                    Cubes[cubeIndex].transform.position = new Vector3(Cubes[cubeIndex].transform.position.x, Cubes[cubeIndex].transform.position.y, CubesZ[cubeIndex]);
+                    rb = Cubes[cubeIndex].GetComponent<Rigidbody>();
+                    rb.isKinematic = false;     
+                }
+            }  
 
             if (!state3D)
             {
                 cam.transform.position = player.position + new Vector3(0, 4, -300);
                 cam.transform.rotation = Quaternion.Euler(0, 0, 0);
                 cam.orthographicSize = cam2DSize;
+                for (int cubeIndex = 0; cubeIndex < Cubes.Length; cubeIndex++)
+                {
+                    CubesZ[cubeIndex] = Cubes[cubeIndex].transform.position.z;
+                    rb = Cubes[cubeIndex].GetComponent<Rigidbody>();
+                    rb.isKinematic = true;
+                    Debug.Log(CubesZ[cubeIndex]);
+                    Cubes[cubeIndex].transform.position = new Vector3(Cubes[cubeIndex].transform.position.x, Cubes[cubeIndex].transform.position.y, player.position.z);
+                }
             }
         }
 
         if (!state3D)
-        {
+        {          
             cam.transform.position = player.position + new Vector3(0, 4, -300);
         }
     }
