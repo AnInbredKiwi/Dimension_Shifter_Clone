627e8cf78d81f81ca24613d324c01dfc7bd21589
Juan <juan.osorio@ue-germany.de>
2021-11-07 14:54:27 +0100

    Rebuild the Dimension Shift System to work with 2D and 3D colliders

--- Code Name 3-1G/Assets/Scripts/GameState.cs
+++ Code Name 3-1G/Assets/Scripts/GameState.cs
@@ -5,82 +5,208 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
-    // I was gonna use an enum but there are just 2 states, a bool should do LOL
-    public static bool state3D;
-
+    public enum GameStates
+    {
+        ThreeD,
+        TwoD,
+        Transition
+    }
+    public static GameStates currentState;
+    public static GameStates previousState;
     Camera cam;
+    public float zPosition2D;
+    public float spaceBetweenRaycasts;
     GameObject player;
-    GameObject[] Cubes;
-    float[] CubesZ;
+    Transform player2D;
+    GameObject[] environment;
 
-    public static Transform currentGroundCube;
+    public float camYOffset2D = 3f;
 
     public float cam2DSize = 7f;
 
+    public float ground2dRaycastDistance = 1.5f;
 
     void Awake()
     {
-        state3D = true;
+        currentState = GameStates.ThreeD;
         cam = FindObjectOfType<Camera>();
         player = GameObject.FindGameObjectWithTag("Player");
-        Cubes = GameObject.FindGameObjectsWithTag("Cube");
-        CubesZ = new float[Cubes.Length];
-
-
+        player2D = player.transform.GetChild(1);
+        environment = GameObject.FindGameObjectsWithTag("Environment");
+        Debug.Log(environment);
     }
 
     void Update()
     {
-
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            state3D = !state3D;
-            cam.orthographic = !cam.orthographic;
-            cam.GetComponent<CinemachineBrain>().enabled = !cam.GetComponent<CinemachineBrain>().enabled;
-            //player.transform.GetChild(0).GetComponent<Collider>().enabled = !player.transform.GetChild(0).GetComponent<Collider>().enabled;
-
-            Rigidbody rb;
-
-            if (state3D)
-            {
-                for (int cubeIndex = 0; cubeIndex < Cubes.Length; cubeIndex++)
-                {
-                    Cubes[cubeIndex].transform.position = new Vector3(Cubes[cubeIndex].transform.position.x, Cubes[cubeIndex].transform.position.y, CubesZ[cubeIndex]);
-                    rb = Cubes[cubeIndex].GetComponent<Rigidbody>();
-                    if (rb != null)
-                        rb.isKinematic = false;
-                    if (currentGroundCube != null)
-                    {
-                        Debug.Log("Moving to cube " + currentGroundCube);
-                        player.GetComponent<CharacterController>().enabled = false;
-                        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, currentGroundCube.position.z);
-
-                        player.GetComponent<CharacterController>().enabled = true;
-                    }
-                }
-            }
-
-            if (!state3D)
-            {
-                player.GetComponent<ThirdPersonMovement>().ReleaseCube();
-                cam.transform.position = player.transform.position + new Vector3(0, 4, -300);
-                cam.transform.rotation = Quaternion.Euler(0, 0, 0);
-                cam.orthographicSize = cam2DSize;
-                for (int cubeIndex = 0; cubeIndex < Cubes.Length; cubeIndex++)
-                {
-                    CubesZ[cubeIndex] = Cubes[cubeIndex].transform.position.z;
-                    rb = Cubes[cubeIndex].GetComponent<Rigidbody>();
-                    if (rb != null)
-                        rb.isKinematic = true;
-                    Debug.Log(CubesZ[cubeIndex]);
-                    Cubes[cubeIndex].transform.position = new Vector3(Cubes[cubeIndex].transform.position.x, Cubes[cubeIndex].transform.position.y, player.transform.position.z);
-                }
-            }
+            ChangeState(GameStates.Transition);
+            if (currentState == GameStates.Transition && previousState != GameStates.Transition)
+                StartCoroutine("Transition");
+            else
+                Debug.Log("Bruh, where Transition State?");
         }
 
-        if (!state3D)
+        // Camera follow player in 2D
+        if (currentState == GameStates.TwoD)
         {
-            cam.transform.position = player.transform.position + new Vector3(0, 4, -300);
+            cam.transform.position = player2D.position + new Vector3(0, camYOffset2D, zPosition2D);
         }
     }
+
+    IEnumerator Transition()
+    {
+        Debug.Log("Entering transition from state " + previousState);
+
+        cam.orthographic = !cam.orthographic;
+        cam.GetComponent<CinemachineBrain>().enabled = !cam.GetComponent<CinemachineBrain>().enabled;
+
+        if (previousState == GameStates.TwoD)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(player2D.transform.position, Vector2.down, ground2dRaycastDistance, LayerMask.GetMask("Ground"));
+            if (hit != false && hit.collider.gameObject.tag == "Movable")
+            {
+                float zToMove = hit.collider.transform.parent.transform.position.z;
+                player2D.transform.position = new Vector3(player2D.transform.position.x, player2D.transform.position.y, zToMove);
+            }
+        }
+
+        SwitchDimension(player);
+        foreach (GameObject item in environment)
+        {
+            if (previousState == GameStates.ThreeD)
+            {
+                RaycastHit hit;
+                if(Physics.Raycast(new Vector3(item.transform.GetChild(0).position.x, item.transform.GetChild(0).position.y, zPosition2D), item.transform.position - new Vector3(item.transform.GetChild(0).position.x, item.transform.GetChild(0).position.y, zPosition2D).normalized, out hit));
+                {
+                    item.SetActive(false);
+                }
+            }
+            Debug.Log(item.name);
+            SwitchDimension(item);
+        }
+
+        if (previousState == GameStates.ThreeD)
+        {
+            cam.transform.position = player.transform.position + new Vector3(0, 4, -zPosition2D);
+            cam.transform.rotation = Quaternion.Euler(0, 0, 0);
+            cam.orthographicSize = cam2DSize;
+            ChangeState(GameStates.TwoD);
+        }
+
+        else if (previousState == GameStates.TwoD)
+        {
+            ChangeState(GameStates.ThreeD);
+        }
+
+
+        yield return null;
+    }
+
+
+
+    void ChangeState(GameStates newState)
+    {
+        previousState = currentState;
+        currentState = newState;
+        Debug.Log("Switched Game State to " + currentState);
+    }
+
+    void SwitchDimension(GameObject parentObject)
+    {
+        parentObject.SetActive(true);
+        if (previousState == GameStates.ThreeD)
+        {
+            parentObject.transform.position = parentObject.transform.GetChild(0).transform.position;
+            parentObject.transform.GetChild(0).transform.localPosition = Vector3.zero;
+            parentObject.transform.GetChild(0).gameObject.SetActive(false);
+            parentObject.transform.GetChild(1).gameObject.SetActive(true);
+        }
+        if (previousState == GameStates.TwoD)
+        {
+            parentObject.transform.position = parentObject.transform.GetChild(1).transform.position;
+            parentObject.transform.GetChild(1).transform.localPosition = Vector3.zero;
+            parentObject.transform.GetChild(1).gameObject.SetActive(false);
+            parentObject.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    // I was gonna use an enum but there are just 2 states, a bool should do LOL
+    //    public static bool state3D;
+
+    //    Camera cam;
+    //    GameObject player;
+    //    GameObject[] Environment;
+    //    float[] CubesZ;
+
+    //    public static Transform currentGroundCube;
+
+    //    public float cam2DSize = 7f;
+
+
+    //    void Awake()
+    //    {
+    //        state3D = true;
+    //        cam = FindObjectOfType<Camera>();
+    //        player = GameObject.FindGameObjectWithTag("Player");
+    //        Environment = GameObject.FindGameObjectsWithTag("Environment");
+    //        CubesZ = new float[Environment.Length];
+
+
+    //    }
+
+    //    void Update()
+    //    {
+
+    //        if (Input.GetKeyDown(KeyCode.Q))
+    //        {
+    //            state3D = !state3D;
+    //            cam.orthographic = !cam.orthographic;
+    //            cam.GetComponent<CinemachineBrain>().enabled = !cam.GetComponent<CinemachineBrain>().enabled;
+    //            //player.transform.GetChild(0).GetComponent<Collider>().enabled = !player.transform.GetChild(0).GetComponent<Collider>().enabled;
+
+    //            Rigidbody rb;
+
+    //            if (state3D)
+    //            {
+    //                for (int cubeIndex = 0; cubeIndex < Environment.Length; cubeIndex++)
+    //                {
+    //                    Environment[cubeIndex].transform.position = new Vector3(Environment[cubeIndex].transform.position.x, Environment[cubeIndex].transform.position.y, CubesZ[cubeIndex]);
+    //                    rb = Environment[cubeIndex].GetComponent<Rigidbody>();
+    //                    if (rb != null)
+    //                        rb.isKinematic = false;
+    //                    if (currentGroundCube != null)
+    //                    {
+    //                        Debug.Log("Moving to cube " + currentGroundCube);
+    //                        player.GetComponent<CharacterController>().enabled = false;
+    //                        player.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, currentGroundCube.position.z);
+
+    //                        player.GetComponent<CharacterController>().enabled = true;
+    //                    }
+    //                }
+    //            }
+
+    //            if (!state3D)
+    //            {
+    //                player.GetComponent<ThirdPersonMovement>().ReleaseCube();
+    //                cam.transform.position = player.transform.position + new Vector3(0, 4, -300);
+    //                cam.transform.rotation = Quaternion.Euler(0, 0, 0);
+    //                cam.orthographicSize = cam2DSize;
+    //                for (int cubeIndex = 0; cubeIndex < Environment.Length; cubeIndex++)
+    //                {
+    //                    CubesZ[cubeIndex] = Environment[cubeIndex].transform.position.z;
+    //                    rb = Environment[cubeIndex].GetComponent<Rigidbody>();
+    //                    if (rb != null)
+    //                        rb.isKinematic = true;
+    //                    Debug.Log(CubesZ[cubeIndex]);
+    //                    Environment[cubeIndex].transform.position = new Vector3(Environment[cubeIndex].transform.position.x, Environment[cubeIndex].transform.position.y, player.transform.position.z);
+    //                }
+    //            }
+    //        }
+
+    //        if (!state3D)
+    //        {
+    //            cam.transform.position = player.transform.position + new Vector3(0, 4, -300);
+    //        }
+    //    }
 }
--- Code Name 3-1G/Assets/Scripts/ThirdPersonMovement.cs
+++ Code Name 3-1G/Assets/Scripts/ThirdPersonMovement.cs
@@ -29,6 +29,7 @@ public class ThirdPersonMovement : MonoBehaviour
 
     private void Start()
     {
+        cam = GameObject.FindObjectOfType<Camera>().transform;
         controller = GetComponent<CharacterController>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -36,124 +37,118 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
-        isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
-        if (isGrounded && velocity.y < 0)
-            velocity.y = -4f;
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        // Fix movement and jump bug in 2D
-        if (!GameState.state3D)
+        if (GameState.currentState == GameState.GameStates.ThreeD)
         {
-            vertical = 0f;
-            if (Physics.CheckSphere(transform.position + (cam.right * 0.65f) + (Vector3.up * wallDistance), wallDistance, groundMask) && !isGrounded || Physics.CheckSphere(transform.position + (cam.right * 0.65f) + (Vector3.up * (2-wallDistance)), wallDistance, groundMask) && !isGrounded)
-                controller.Move(cam.right * -wallDistance * 1.1f);
-            if (Physics.CheckSphere(transform.position + (cam.right * -0.65f) + (Vector3.up * wallDistance), wallDistance, groundMask) && !isGrounded || Physics.CheckSphere(transform.position + (cam.right * -0.65f) + (Vector3.up * (2-wallDistance)), wallDistance, groundMask) && !isGrounded)
-                controller.Move(cam.right * wallDistance * 1.1f);
-        }
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical);
+            isGrounded = Physics.CheckSphere(transform.position, groundDistance, groundMask);
+            if (isGrounded && velocity.y <= 0)
+                velocity.y = -4f;
 
-        if (direction.magnitude >= 0.1f)
-        {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            float horizontal = Input.GetAxisRaw("Horizontal");
+            float vertical = Input.GetAxisRaw("Vertical");
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+            Vector3 direction = new Vector3(horizontal, 0f, vertical);
 
-        }
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
 
-        velocity.y += gravity * Time.deltaTime;
-
-        controller.Move(velocity * Time.deltaTime);
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
+            }
 
 
+            velocity.y += gravity * Time.deltaTime;
 
-        //if (Input.GetKeyDown(KeyCode.Mouse1))
-        //{
-        //    Debug.Log("Throwing Raycast");
-        //    RaycastHit hit;
-        //    if (Physics.Raycast(transform.position + new Vector3(1, 0, 0), transform.forward, out hit, 2f))
-        //    {
-        //        Debug.Log("Raycast thrown");
-        //        if (hit.transform.tag == "Cube")
-        //        {
-        //            Debug.Log("Launching Cube");
-        //            hit.transform.gameObject.GetComponent<Rigidbody>().AddForce((transform.forward + Vector3.up * 3).normalized * throwForce, ForceMode.VelocityChange);
-        //        }
-        //    }
-        //}
+            controller.Move(velocity * Time.deltaTime);
 
-        if (GameState.state3D)
-        {
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+
+
+            //if (Input.GetKeyDown(KeyCode.Mouse1))
+            //{
+            //    Debug.Log("Throwing Raycast");
+            //    RaycastHit hit;
+            //    if (Physics.Raycast(transform.position + new Vector3(1, 0, 0), transform.forward, out hit, 2f))
+            //    {
+            //        Debug.Log("Raycast thrown");
+            //        if (hit.transform.tag == "Cube")
+            //        {
+            //            Debug.Log("Launching Cube");
+            //            hit.transform.gameObject.GetComponent<Rigidbody>().AddForce((transform.forward + Vector3.up * 3).normalized * throwForce, ForceMode.VelocityChange);
+            //        }
+            //    }
+            //}
+
             Push();
             Grab();
-        }
-    }
 
-    void Push()
-    {
-        Debug.Log("Pushing? " + isPushing);
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-            isPushing = true;
-        else if (Input.GetKeyUp(KeyCode.Mouse0))
-            isPushing = false;
-        RaycastHit hit;
-        if (isPushing && Physics.Raycast(transform.position /*+ transform.forward*0.8f*/ + Vector3.up, transform.forward, out hit, 1f) && grabbedObject == null)
-        {
-            if (hit.transform.tag == "Cube")
-            {
-                Debug.Log("Pushing Cube");
-                hit.transform.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed / 2f;
-            }
         }
-    }
 
-    void Grab()
-    {
-        bool grabbingFrame = false;
-
-        RaycastHit hit;
-        if (Input.GetKeyDown(KeyCode.Mouse1) && Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, 1f) && grabbedObject == null)
+        void Push()
         {
-            grabbedObject = hit.transform.gameObject;
-            grabbedObjectsOGParent = grabbedObject.transform.parent;
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
-            grabbedObject.transform.position = transform.position + transform.forward * 2.5f + Vector3.up * 1.5f;
-            grabbedObject.transform.SetParent(transform);
-            grabbingFrame = true;
-        }
-        if (grabbedObject != null)
-        {
-            if (Input.GetKeyDown(KeyCode.Mouse1) && !grabbingFrame)
-            {
-                ReleaseCube();
-            }
+            Debug.Log("Pushing? " + isPushing);
             if (Input.GetKeyDown(KeyCode.Mouse0))
+                isPushing = true;
+            else if (Input.GetKeyUp(KeyCode.Mouse0))
+                isPushing = false;
+            RaycastHit hit;
+            if (isPushing && Physics.Raycast(transform.position /*+ transform.forward*0.8f*/ + Vector3.up, transform.forward, out hit, 1f) && grabbedObject == null)
             {
-                ReleaseCube();
+                if (hit.transform.tag == "Movable")
+                {
+                    Debug.Log("Pushing Movable Object");
+                    hit.transform.gameObject.GetComponent<Rigidbody>().velocity = transform.forward * speed / 2f;
+                }
             }
         }
-    }
 
-    public void ReleaseCube()
-    {
-        if (grabbedObject != null)
+        void Grab()
         {
-            grabbedObject.transform.SetParent(grabbedObjectsOGParent);
-            grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
-            grabbedObject.transform.rotation = Quaternion.Euler(0, 0, 0);
-            if (Input.GetKeyDown(KeyCode.Mouse0))
-                grabbedObject.GetComponent<Rigidbody>().AddForce((transform.forward + Vector3.up * 3).normalized * throwForce, ForceMode.VelocityChange);
-            grabbedObject = null;
+            bool grabbingFrame = false;
+
+            RaycastHit hit;
+            if (Input.GetKeyDown(KeyCode.Mouse1) && Physics.Raycast(transform.position + Vector3.up, transform.forward, out hit, 1f) && grabbedObject == null)
+            {
+                grabbedObject = hit.transform.gameObject;
+                grabbedObjectsOGParent = grabbedObject.transform.parent;
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                grabbedObject.transform.position = transform.position + transform.forward * 2.5f + Vector3.up * 1.5f;
+                grabbedObject.transform.SetParent(transform);
+                grabbingFrame = true;
+            }
+            if (grabbedObject != null)
+            {
+                if (Input.GetKeyDown(KeyCode.Mouse1) && !grabbingFrame)
+                {
+                    ReleaseCube();
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                {
+                    ReleaseCube();
+                }
+            }
+        }
+
+        void ReleaseCube()
+        {
+            if (grabbedObject != null)
+            {
+                grabbedObject.transform.SetParent(grabbedObjectsOGParent);
+                grabbedObject.transform.SetSiblingIndex(0);
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.transform.rotation = Quaternion.Euler(0, 0, 0);
+                if (Input.GetKeyDown(KeyCode.Mouse0))
+                    grabbedObject.GetComponent<Rigidbody>().AddForce((transform.forward + Vector3.up * 3).normalized * throwForce, ForceMode.VelocityChange);
+                grabbedObject = null;
+            }
         }
     }
 }
